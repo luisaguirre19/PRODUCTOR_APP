9d757b1495764ec8ed8884af016978304c64d001
Luis Aguirre <luisaguirre_19@yahoo.es>
2023-05-12 23:12:19 -0600

    path

--- src/app/servicios/sql.service.ts
+++ src/app/servicios/sql.service.ts
@@ -8,8 +8,11 @@ import { Observable, throwError, from } from 'rxjs';
 })
 export class SqlService {
  // base_path:string = 'https://coffee-benef.azurewebsites.net/api/count'
-  base_path:string = 'http://localhost:8097/api/'
-  base_path_beneficio:string = 'http://localhost:8094/api/'
+  //base_path:string = 'http://localhost:8097/api/'
+  //base_path_beneficio:string = 'http://localhost:8094/api/'
+
+  base_path:string = 'coffee-dv.azurewebsites.net/api/'
+  base_path_beneficio:string = 'coffee-benef.azurewebsites.net/api/'
 
   httpOptions = {
     headers: new HttpHeaders({
